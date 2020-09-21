7fc091c75a6dc61434c57c1766de8bab61d4ab7d
TRI AFRILIYANTI <71365606+triafri@users.noreply.github.com>
2020-09-21 10:45:38 +0700

    Add files via upload

---
+++ tugas17_1.js
@@ -0,0 +1,23 @@
+function ubahLower(){
+	var kal = "Saya beLajar bahaSa peMrograman deNgan khUsuk"
+	console.log(kal.toLowerCase())
+}
+ubahLower();
+
+function ubahUpper(){
+	var kal = "Saya beLajar bahaSa peMrograman deNgan khUsuk"
+	console.log(kal.toUpperCase())
+}
+ubahUpper();
+
+function ubahLow(){
+	var kal = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual"
+	console.log(kal.toLowerCase())
+}
+ubahLow();
+
+function ubahUp(){
+	var kal = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual"
+	console.log(kal.toUpperCase())
+}
+ubahUp();
